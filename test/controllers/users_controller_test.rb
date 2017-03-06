f2d4b8d9b5f142f790b7c8cd505bf84993c29aa1
khosrobeygizohre <khosrobeygi.zohre@yahoo.com>
2017-03-06 14:59:55 -0800

    Finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
+  test "should get show" do
+    get users_show_url
     assert_response :success
   end
 
