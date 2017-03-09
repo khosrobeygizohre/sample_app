a69529fc6edafd3770f5030a9de7f7bb382bdb0a
khosrobeygizohre <khosrobeygi.zohre@yahoo.com>
2017-03-09 00:47:53 -0800

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-def hello
-    render html: "hello, world! , zohreb khosrobeygi"
-  end
+  include SessionsHelper
 end
